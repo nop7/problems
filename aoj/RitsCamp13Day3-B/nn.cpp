7311e9c555edbe90ea9c3ea81ab41572a655e70e
naruhito <naruhito78@gmail.com>
2013-05-24 00:33:02 +0900

    retry: RitsCamp13Day3-B / nn.cpp

--- aoj/RitsCamp13Day3-B/nn.cpp
+++ aoj/RitsCamp13Day3-B/nn.cpp
@@ -14,56 +14,38 @@ using namespace std;
 #define EPS 1e-7
 #define INF 1e9
 
-const int MAXW=400;
-const int MAXN=400;
+const int MAXW=400; //393
+const int MAXN=393;
+const int MAXM=393;
 int s[MAXN];
 int l[MAXN];
 int p[MAXN];
-int res[MAXW];
-int ans[MAXW];
+
+int dp[MAXW];
+int res[MAXM];
 
 int main() {
-    int n;
-    cin>>n;
+    int n; cin>>n;
     REP(i,n) cin>>s[i]>>l[i]>>p[i];
-    fill(res,res+MAXW,-1);
-    res[0] = 0;
 
-    REP(i,MAXW) if(res[i] >= 0) {
-        REP(j,n) {
-            REP(k,l[j]-s[j]+1) {
-                int len = k+s[j];
-                if(i+len>=MAXW) {
-                    continue;
-                }
-                res[i+len] = max(res[i+len], res[i]+p[j]);
-            }
+    fill(dp, dp+MAXW, -1);
+    dp[0]=0;
+
+    REP(i,MAXW) if(dp[i]!=-1) {
+        REP(j,n) REP(k,l[j]-s[j]+1) if(i+s[j]+k < MAXW) {
+            dp[i+s[j]+k] = max(dp[i+s[j]+k], dp[i]+p[j]);
         }
     }
 
-    int m;
-    cin>>m;
-    bool pass = true;
-
+    int m; cin>>m;
+    bool ok=true;
     REP(i,m) {
-        int w;
-        cin>>w;
-        if(res[w] < 0) {
-            pass = false;
-            break;
-        }
-        else {
-            ans[i] = res[w];
-        }
+        int w; cin>>w;
+        if(dp[w] == -1) {ok=false;break;}
+        res[i] = dp[w];
     }
+    if(ok) REP(i,m) cout << res[i] << endl;
+    else cout << -1 << endl;
 
-    if(pass) {
-        REP(i,m) {
-            cout << ans[i] << endl;
-        }
-    }
-    else {
-        cout << -1 << endl;
-    }
     return 0;
 }
